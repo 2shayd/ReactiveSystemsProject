b316129618bc59c82c89512ab9a135981b477e36
realjesse <154276383+realjesse@users.noreply.github.com>
2026-06-11 20:22:32 -0700

    Add getCrashEvents() to CrashEventRemoteDataSource

--- app/src/main/java/com/example/noimo/data/remote/datasource/CrashEventRemoteDataSource.kt
+++ app/src/main/java/com/example/noimo/data/remote/datasource/CrashEventRemoteDataSource.kt
@@ -4,4 +4,6 @@ import com.example.noimo.data.remote.dto.CrashEventRemoteDto
 
 interface CrashEventRemoteDataSource {
     suspend fun uploadCrashEvents(events: List<CrashEventRemoteDto>)
+
+    suspend fun getCrashEvents(): List<CrashEventRemoteDto>
 }
